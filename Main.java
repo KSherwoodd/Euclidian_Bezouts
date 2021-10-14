f741016bc896527c4ab8c1b59cc4c68a28aa6881
Kingsley Sherwood <92546532+KSherwoodd@users.noreply.github.com>
2021-10-14 21:27:07 +0000

    Removed unnecessary queues, fixed algorithm

--- Main.java
+++ Main.java
@@ -2,23 +2,19 @@ import java.util.Scanner;import java.util.Queue;import java.util.LinkedList;impo
 
 class Main {
   public static int HCF(int a, int b) {
-    Queue<Integer> q = new LinkedList<>();
-    Queue<Integer> r = new LinkedList<>();
     Queue<Integer> s = new LinkedList<>(List.of(1, 0));
     Queue<Integer> t = new LinkedList<>(List.of(0, 1));
-    //int i=0;
+    int quot=0;
     while (b != 0) {
       int x = b;
       b = a % b;
-      int rema = b;
-      r.add(rema);
-      if (b != 0) {int quot = a / b;q.add(quot);}
+      if (b != 0) {quot = a / x;}
+      s.add(s.remove()-quot*s.peek());
+      t.add(t.remove()-quot*t.peek());
       a = x;
-      s.add(s.remove()-q.peek()*s.peek());
-      t.add(t.remove()-q.peek()*t.peek());
-      //i++;
+      /*System.out.println();
+      System.out.println(quot + " " + s.peek() + " " + t.peek());*/
     }
-    System.out.println(s.poll() + " " + t.poll());
     return a;
   }
   public static void main(String[] args) {
