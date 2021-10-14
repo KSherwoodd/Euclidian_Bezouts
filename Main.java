8d45ff9d6f221b40f8affb2d0137cedc7d5cb0b8
Kingsley Sherwood <>
2021-10-14 20:35:48 +0000

    Addition of s / t, beginning logic for s, t

--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
-import java.util.Scanner;import java.util.Queue;import java.util.LinkedList;
+import java.util.Scanner;import java.util.Queue;import java.util.LinkedList;import java.util.List;
+
 class Main {
   public static int HCF(int a, int b) {
-    Queue<Integer> quotients = new Queue<Integer>(new int[] {0, 1});
-    Queue<Integer> remainders = new LinkedList<>();
+    Queue<Integer> q = new LinkedList<>();
+    Queue<Integer> r = new LinkedList<>();
+    Queue<Integer> s = new LinkedList<>(List.of(1, 0));
+    Queue<Integer> t = new LinkedList<>(List.of(0, 1));
+    int i=0;
     while (b != 0) {
       int x = b;
       b = a % b;
-      quotients.add(b);
-      if (b != 0) {int q = a / b;}
+      int r = b;
+      r.add(r);
+      if (b != 0) {int quot = a / b;q.add(q);}
       a = x;
+      s.add(s.poll()-q.peek());
+      i++;
     }
     return a;
   }
@@ -18,5 +25,6 @@ class Main {
     int a = input.nextInt();
     int b = input.nextInt();
     System.out.println(HCF(a, b));
+    input.close();
   }
 }
