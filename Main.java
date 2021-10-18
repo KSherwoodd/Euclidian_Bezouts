f0c18a927a78ef37d4fb61df207901a270315aca
Kingsley Sherwood <92546532+KSherwoodd@users.noreply.github.com>
2021-10-18 15:21:00 +0000

    Began converting to polynomials, created arrays

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 class Main {
-  public static int HCF(int a, int b) {
+  public static int HCF(int[][] a, int[][] b) {
     Queue<Integer> s = new LinkedList<>(List.of(1, 0));
     Queue<Integer> t = new LinkedList<>(List.of(0, 1));
     int quot=0;
@@ -23,12 +23,16 @@ class Main {
     return a;
   }
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    /*Scanner input = new Scanner(System.in);
     System.out.println("A: ");
     int a = input.nextInt();
     System.out.println("B: ");
     int b = input.nextInt();
-    input.close();    
+    input.close();*/ 
+    int[][] a = new int[5][2];
+    int[][] b = new int[4][2];
+    //3x^4 + 5x^3 + 2x^2 + x + 9 => {{3,4},{5,3},{2,2},{1,1},{9,0}}
+    //In the form [coefficient][indice]
     int hcf = HCF(a, b);
     System.out.println("\nHighest Common Factor: ");
     System.out.println(hcf);
