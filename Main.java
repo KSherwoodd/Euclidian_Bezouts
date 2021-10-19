809a3dceea7b255196201e8eb36271f482218557
Kingsley Sherwood <92546532+KSherwoodd@users.noreply.github.com>
2021-10-19 14:53:41 +0000

    beginning simpler implementation

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+/*https://en.wikipedia.org/wiki/Synthetic_division#Expanded_synthetic_division
+depreciates most all of the work i was going to do
+
+But not completely, going to implement full synth division*/
+
 import java.util.Scanner;
 import java.util.Queue;
 import java.util.LinkedList;
@@ -5,17 +10,19 @@ import java.util.List;
 
 class Main {
 
-  public static int[][] Poly_Remainder(int[][] a, int[][] b) {
-    
+  public static int[] poly_remainder(int[] a, int[] b) {
+    for (int i : b) {
+
+    }
   }
 
-  public static int[][] HCF(int[][] a, int[][] b) {
+  public static int[] HCF(int[] a, int[] b) {
     Queue<Integer> s = new LinkedList<>(List.of(1, 0));
     Queue<Integer> t = new LinkedList<>(List.of(0, 1));
     int quot=0;
     while (b != 0) {
       int x = b;
-      b = a % b;
+      //b = a % b; depreciated by Poly_Remainder
       if (b != 0) {quot = a / x;}
       s.add(s.remove()-quot*s.peek());
       t.add(t.remove()-quot*t.peek());
@@ -34,11 +41,13 @@ class Main {
     System.out.println("B: ");
     int b = input.nextInt();
     input.close();*/ 
-    int[][] a = new int[5][2];
-    int[][] b = new int[4][2];
+    int[] a = {3,5,2,1,9};
+    int[] b = {4,6,7,2};
     //3x^4 + 5x^3 + 2x^2 + x + 9 => {{3,4},{5,3},{2,2},{1,1},{9,0}}
     //In the form [coefficient][indice]
-    int hcf = HCF(a, b);
+    //or skip the indicies entirely, just use the length then it can be 1D
+    //{3,5,2,1,9}
+    int[] hcf = HCF(a, b);
     System.out.println("\nHighest Common Factor: ");
     System.out.println(hcf);
   }
